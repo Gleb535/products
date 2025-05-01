93835ad4065ab6fae187e0e7f71ea613d8c24ecd
Gruzdev Gleb <gruzdev.g.a@edu.mirea.ru>
2025-05-01 15:30:45 +0300

    danya check it pls

--- product.cpp
+++ product.cpp
@@ -27,6 +27,13 @@ public:
                   << "Y coord: " << y << "\n"
                   << "Z coord: " << z << std::endl;
     }
+
+    void get_product(int &a, int &b, int &c)
+    {
+        a = x;
+        b = y;
+        c = z;
+    }
 };
 
 class Line : public Dot
@@ -516,7 +523,7 @@ public:
     Node(T new_tovar) : tovar(new_tovar), prev(NULL), next(NULL) {}
 };
 
-// двусвяз список
+// двусвяз список для продуктов
 template <typename F>
 class linked_list
 {
@@ -785,52 +792,155 @@ public:
             current = current->next;        // переходим к следующему
         }
     }
+};
 
-    // метод отображения списка по фильтру id
-    void print_list_by_id(int id) const
+// лист для точек
+
+template <>
+class linked_list<Dot>
+{
+private:
+    Node<Dot> *head; // указатель на первый узел списка
+    Node<Dot> *tail; // указатель на последний узел списка
+
+public:
+    // конструктор по умолчанию
+    linked_list<Dot>() : head(NULL), tail(NULL) {}
+
+    // конструктор копирования
+    linked_list<Dot>(const linked_list<Dot> &other) : head(nullptr), tail(nullptr)
     {
-        Node<F> *current = head;
-        while (current) // пока не достигнут конец списка
+        Node<Dot> *current = other.head; // начинаем с головы другого списка
+        while (current)                  // пока не достигнем конца списка
         {
-            if (current->tovar.get_id() == id) // проверка на соответствию с фильтром
-            {
-                current->tovar.print_product(); // вывод инфы товара
-            }
-            current = current->next; // переходим к следующему
+            add_product(current->tovar); // добавляем товар из текущего узла в новый список
+            current = current->next;     // переход к следующему
+        }
+    }
+    // конструктор перемещения
+    linked_list<Dot>(linked_list<Dot> &&other) : head(other.head), tail(other.tail)
+    {
+        other.head = nullptr; // очищаем указатель на голову в исходном объекте
+        other.tail = nullptr; // очищаем указатель на хвост в исходном объекте
+    }
+
+    // перегруженный оператор сложения для добавления товара
+    linked_list<Dot> &operator+(const Node<Dot> &product)
+    {
+        add_product(product); // добавляем товар в список
+        return *this;         // возвращаем ссылку на текущий объект (для поддержания цепочки вызовов)
+    }
+
+    // метод добавления товара в список
+    void add_product(const Node<Dot> &new_product)
+    {
+        Node<Dot> *new_node = new Node<Dot>(new_product); // создаем новый узел, используя конструктор node
+        if (!head)                                        // проверка пуст ли список (head == nullptr)
+        {
+            head = tail = new_node; // если список пустой, новый узел становится и головой, и хвостом (начальная точка списка)
+        }
+        else
+        {
+            tail->next = new_node; // у текущего хвоста устанавливаем указатель на новый узел
+            new_node->prev = tail; // у нового узла устанавливаем указатель на предыдущий узел (хвост)
+
+            tail = new_node; // обновляем хвост списка, делая его текущим новым узлом
         }
     }
 
-    // метод отображения списка по фильтру цены
-    void print_list_by_price(int price) const
+    // функция для вставки нового узла в заданную позицию
+    Node<Dot> *insertAtPos(Node<Dot> *head, int pos, Node<Dot> new_data)
     {
-        Node<F> *current = head; // начинаем с головы списка
-        while (current)          // пока не достигнем конца списка
+
+        Node<Dot> *new_node = new Node<Dot>(new_data); // создаем новый узел
+
+        if (pos == 1) // вставка в начало
         {
-            if (current->tovar.get_price() == price) // проверка на соответствию с фильтром
-            {
-                current->tovar.print_product(); // вывод инфы товара
-            }
-            current = current->next; // переходим к следующему
+            new_node->next = head;
+
+            // если список не пуст, устанавливаем prev головы на новый узел
+            if (head != NULL)
+                head->prev = new_node;
+
+            // устанавливаем новый узел как голову связанного списка
+            head = new_node;
+            return head;
         }
+
+        Node<Dot> *curr = head;
+        // перемещаемся по списку, чтоб найти узел перед позицией вставки
+        for (int i = 1; i < pos - 1 && curr != NULL; ++i)
+        {
+            curr = curr->next;
+        }
+
+        // если позиция вне границ
+        if (curr == NULL)
+        {
+            delete new_node;
+            return head;
+        }
+
+        new_node->prev = curr; // устанавливаем prev нового узла на текущий
+
+        new_node->next = curr->next; // устанавливаем next нового узла на следующий от текущего
+
+        curr->next = new_node; // обновляем next текущего узла на новый узел
+
+        // если новый узел не последний, обновляем prev следующего узла на новый узел
+        if (new_node->next != NULL)
+            new_node->next->prev = new_node;
+
+        return head; // возвращаем голову двусвяз списка
     }
 
-    // метод отображения списка по фильтру поставщика
-    void print_list_by_plug(plugs plug) const
+    // сохранение списка товаров в файл
+    void save_to_file(const std::string &filename)
     {
-        Node<F> *current = head; // начинаем с головы списка
-        while (current)          // пока не достигнем конца списка
+        std::ofstream file(filename); // открываем файл для записи
+        Node<Dot> *current = head;    // начинаем с головы списка
+        while (current)               // пока не достигнем конца списка
         {
-            if (current->tovar.get_plug() == plug) // проверка на соответствию с фильтром
+            int x, y, z;
+            current->tovar.get_product(x, y, z);            // получаем данные о товаре из текущего узла
+            file << x << " " << y << " " << z << std::endl; // записываем данные в файл
+            current = current->next;                        // переходим к следующему узлу
+        }
+        file.close(); // закрываем файл после записи
+    }
+
+    // метод загрузки списка товаров из файла
+    void load_from_file(const std::string &filename)
+    {
+        std::ifstream file(filename); // открываем файл для чтения
+        while (!file.eof())           // продолжаем до конца файла
+        {
+            int x, y, z;
+            file >> x >> y >> z; // читаем данные из файла
+            if (file)            // если данные были успешно прочитаны
             {
-                current->tovar.print_product(); // вывод инфы товара
+                Dot new_product(x, y, z); // создаем новый товар
+                add_product(new_product); // добавляем товар в список
             }
-            current = current->next; // переходим к следующему
+        }
+        file.close(); // закрываем файл после чтения
+    }
+
+    // вывод списка товаров
+    void print_list() const
+    {
+        Node<Dot> *current = head; // начинаем с головы списка
+        while (current)            // пока не достигнем конца списка
+        {
+            current->tovar.print_product(); // вывод инфы товара
+            current = current->next;        // переходим к следующему
         }
     }
 };
 
 int main()
 {
+    /*
     int user_id;
     std::cin >> user_id;
 
@@ -860,14 +970,18 @@ int main()
         std::cout << e.what() << '\n';
     }
 
+    */
+
     linked_list<Product> pl;
 
     pl.add_product(Product(1, "Product1", 100, SLAY));
 
     linked_list<Dot> dd;
-    // dd.add_product(Dot());
-    // dd.add_product(Dot(1, 1, 1));
-    // dd.print_list();
+    dd.add_product(Dot());
+    dd.add_product(Dot(1, 1, 1));
+    // dd.save_to_file("products.txt");
+    // dd.load_from_file("products.txt");
+    dd.print_list();
     //   выводим первый вариант списка
     //  pl.print_list();
     std::cout << "\n";
@@ -876,7 +990,7 @@ int main()
     linked_list<Parall> l;
     l.add_product(Parall());
     l.add_product(Parall());
-    l.print_list();
+    // l.print_list();
 
     // pl.add_product(Product());
 
